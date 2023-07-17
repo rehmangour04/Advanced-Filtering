e6e6cd18f2859e4989017f3097b4f269883d00bf
Ataur Rehman <rehmangour04@gmail.com>
2023-07-17 23:16:36 +0530

    complete

---
+++ src/Products/Products.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import "./Products.css";
+
+const Products = ({ result }) => {
+  return (
+    <>
+      <section className="card-container">{result}</section>
+    </>
+  );
+};
+
+export default Products;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
